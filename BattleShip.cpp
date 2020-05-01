10d3500480a7d1e4abd9c5498b7276fb924e97ed
Montelto <55961290+Montelto@users.noreply.github.com>
2020-05-01 11:56:46 -0500

    Update BattleShip.cpp

--- BattleShip.cpp
+++ BattleShip.cpp
@@ -1,3 +1,11 @@
+//
+// Joseph McFarland
+// 9072511679
+// mcfarland3@wis.edu
+// https://canvas.wisc.edu/courses/188843/files for slides
+// Made in CLion with commits to GitHub
+//
+
 #include "BattleShip.h"
 
 int main()
